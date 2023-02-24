2892ae6205aa7e5c29adf6165adddcefe7cba1bb
Joel Strasser <strasser999@gmail.com>
2023-02-24 23:29:13 +0100

    add main_test.go

---
+++ main_test.go
@@ -0,0 +1,9 @@
+//
+// Copyright (c) 2023 Joel Strasser <joelstrasser1@gmail.com>
+//
+// Licensed under the EUPL-1.2 license.
+//
+// For the full license text consult the 'LICENSE' file from the repository.
+//
+
+package main
