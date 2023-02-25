a7b08aa7d8767ae48b1f9466c1d44fd043d506f5
Joel Strasser <strasser999@gmail.com>
2023-02-25 20:07:57 +0100

    test encryption and decryption

--- main_test.go
+++ main_test.go
@@ -7,3 +7,19 @@
 //
 
 package main
+
+import (
+	"enka/enka_decryption"
+	"enka/enka_encryption"
+	"testing"
+)
+
+func TestDefaultEncryption(t *testing.T) {
+	args := []string{"--algo", "aes256cbc", "--kdf", "pbkdf2:650000:sha256", "--key", "1234", "--text", "abcd"}
+	enka_encryption.Encrypt(args, nil, nil)
+}
+
+func TestDefaultDecryption(t *testing.T) {
+	args := []string{"--string", "%enka%v1%aes256cbc%pbkdf2:650000:sha256%rhGye/PFC9U=%GiGjGRW2BrQxYdiY69Dstg==%BVeCM81SXQqSwVuMjRyIIQ==", "--key", "1234"}
+	enka_decryption.Decrypt(args, nil, nil)
+}
